7cba2fc92aaa2bb852104dd03688a4e87144585d
Taoling Yang <ytlty516@gmail.com>
2015-10-21 19:36:42 -0400

    Update Utils_prime_letter_to_number_dict.py

--- Utils_prime_letter_to_number_dict.py
+++ Utils_prime_letter_to_number_dict.py
@@ -1,4 +1,4 @@
-letters_to_prime_dict = \
+letters_to_prime_dict =\
     {'a':2, 'b':3, 'c':5, 'd':7, 'e':11, 'f':13,\
     'g':17, 'h':19, 'i':23, 'j':29, 'k':31,\
     'l':37, 'm':41, 'n':43, 'o':47, 'p':53,\
